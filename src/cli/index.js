1a501d250c1b0712e99b2492713549163bc6d848
James Lefrère <james@candis.io>
2018-02-14 15:49:18 +0100

    Control Ganache and wink

--- src/cli/index.js
+++ src/cli/index.js
@@ -3,6 +3,7 @@
 import yargs from 'yargs';
 
 import TrufflePigUI from './trufflepigui';
+import { DEFAULT_GANACHE_PORT } from '../constants';
 
 const args = yargs
   .usage('$0 path/to/my/contracts [another/path, ...]')
@@ -12,12 +13,6 @@ const args = yargs
       describe: 'Port to serve the contracts from',
       type: 'number',
       default: 3030,
-    },
-    e: {
-      alias: 'endpoint',
-      describe: 'API endpoint to serve the contracts from',
-      type: 'string',
-      default: 'contracts',
     },
     v: {
       alias: 'verbose',
@@ -31,14 +26,33 @@ const args = yargs
       type: 'string',
       default: './build/contracts',
     },
+    gp: {
+      alias: 'ganachePort',
+      describe: 'Port to run Ganache on',
+      type: 'number',
+      default: DEFAULT_GANACHE_PORT,
+    },
+    sg: {
+      alias: 'startGanache',
+      describe: 'Flag for whether to start Ganache',
+      type: 'boolean',
+      default: true,
+    },
   })
   .parse();
 
-const pig = new TrufflePigUI({
-  contractDir: String(args.contractDir),
-  port: parseInt(args.port, 10),
-  endpoint: String(args.endpoint),
-  verbose: !!args.verbose,
-});
+const pig = new TrufflePigUI(
+  {
+    contractDir: String(args.contractDir),
+    port: parseInt(args.port, 10),
+    verbose: !!args.verbose,
+  },
+  {
+    port: parseInt(args.ganachePort, 10),
+    startGanache: !!args.startGanache,
+  },
+);
 
-pig.start();
+(async () => {
+  await pig.start();
+})();
