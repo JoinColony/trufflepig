ca75e26f0f2f588fb9db12c25c4299635811f7b1
James Lefrère <james@candis.io>
2018-02-14 15:48:10 +0100

    Define constants

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+export const CONFIG_ENDPOINT = '/_config';
+export const CONTRACTS_ENDPOINT = '/contracts';
+export const CORS_WHITELIST = ['0.0.0.0', '127.0.0.1', '[::1]', 'localhost'];
+export const DEFAULT_GANACHE_PORT = 8545;
+export const DEFAULT_PIG_PORT = 3030;
